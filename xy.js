90804456fd6d597cde3f1b5c1bbf591a772a42c2
Sushmith Thuluva <42782828+ST797@users.noreply.github.com>
2020-06-22 12:53:03 +0530

    Add files via upload

---
+++ xy.js
@@ -0,0 +1,30 @@
+
+
+function myPlay(){
+    var audio = new Audio("batman.mp3");
+    audio.play();
+}
+
+function myPlay1(){
+    var audio = new Audio("superman.mp3");
+    audio.play();
+}
+
+function myPlay2(){
+    var audio = new Audio("ww.mp3");
+    audio.play();
+}
+
+function myPlay3(){
+    var audio = new Audio("gl.mp3");
+    audio.play();
+}
+
+function myPlay4(){
+    var audio = new Audio("flash.mp3");
+    audio.play();
+}
+function myPlay5(){
+    var audio = new Audio("aquaman.mp3");
+    audio.play();
+}
